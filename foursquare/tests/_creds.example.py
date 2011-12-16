5ac0de34da6844fc04a5a67343d1722fc2e88dae
mLewisLogic <mike@fondu.com>
2011-12-16 13:07:33 -0500

    added _creds.example.py

---
+++ foursquare/tests/_creds.example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# (c) 2011 Mike Lewis
+
+CLIENT_ID = u'YOUR_CLIENT_ID'
+CLIENT_SECRET = u'YOUR_CLIENT_SECRET'
+ACCESS_TOKEN = u'EXAMPLE_USER_ACCESS_TOKEN'
