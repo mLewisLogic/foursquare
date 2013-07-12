a21c86d90097fe12395772f2337ed5bd71ee3f4e
Mike Lewis <mike.lewis@airbnb.com>
2013-07-12 09:30:04 -0700

    support for test creds via os.environ

--- foursquare/tests/__init__.py
+++ foursquare/tests/__init__.py
@@ -2,14 +2,20 @@
 # -*- coding: UTF-8 -*-
 # (c) 2013 Mike Lewis
 
+import os
 import unittest
 
 import foursquare
 
-try:
-    from . import _creds
-except ImportError:
-    print "Please create a creds.py file in this package, based upon creds.example.py"
+if 'CLIENT_ID' in os.environ and 'CLIENT_SECRET' in os.environ and 'ACCESS_TOKEN' in os.environ:
+    CLIENT_ID = os.environ['CLIENT_ID']
+    CLIENT_SECRET = os.environ['CLIENT_SECRET']
+    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+else:
+    try:
+        from _creds import *
+    except ImportError:
+        print "Please create a creds.py file in this package, based upon creds.example.py"
 
 
 
@@ -31,23 +37,35 @@ class BaseEndpointTestCase(unittest.TestCase):
 class BaseAuthenticationTestCase(BaseEndpointTestCase):
     def setUp(self):
         self.api = foursquare.Foursquare(
-            client_id=_creds.CLIENT_ID,
-            client_secret=_creds.CLIENT_SECRET,
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
             redirect_uri='http://example.org'
         )
 
 class BaseAuthenticatedEndpointTestCase(BaseEndpointTestCase):
     def setUp(self):
         self.api = foursquare.Foursquare(
-            client_id=_creds.CLIENT_ID,
-            client_secret=_creds.CLIENT_SECRET,
-            access_token=_creds.ACCESS_TOKEN
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            access_token=ACCESS_TOKEN
         )
 
 class BaseUserlessEndpointTestCase(BaseEndpointTestCase):
     def setUp(self):
         self.api = foursquare.Foursquare(
-            client_id=_creds.CLIENT_ID,
-            client_secret=_creds.CLIENT_SECRET,
-            access_token=_creds.ACCESS_TOKEN
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            access_token=ACCESS_TOKEN
         )
+
+class MultilangEndpointTestCase(BaseEndpointTestCase):
+    def setUp(self):
+        self.apis = []
+        for lang in ('es', 'fr', 'de', 'it', 'ja', 'th', 'ko', 'ru', 'pt', 'id'):
+            self.apis.append(
+                foursquare.Foursquare(
+                    client_id=CLIENT_ID,
+                    client_secret=CLIENT_SECRET,
+                    lang=lang
+                )
+            )
--- foursquare/tests/test_lang.py
+++ foursquare/tests/test_lang.py
@@ -3,29 +3,17 @@
 # (c) 2013 Mike Lewis
 import logging; log = logging.getLogger(__name__)
 
-import foursquare
-
-try:
-    from . import _creds
-except ImportError:
-    print "Please create a creds.py file in this package, based upon creds.example.py"
-
-from . import BaseEndpointTestCase
+from . import MultilangEndpointTestCase
 
 
 
-class MultiLangTestCase(BaseEndpointTestCase):
+class MultiLangTestCase(MultilangEndpointTestCase):
     """
     General
     """
     def test_lang(self):
         """Test a wide swath of languages"""
-        for lang in ('es', 'fr', 'de', 'it', 'ja', 'th', 'ko', 'ru', 'pt', 'id'):
-            client = foursquare.Foursquare(
-                client_id=_creds.CLIENT_ID,
-                client_secret=_creds.CLIENT_SECRET,
-                lang=lang
-            )
-            categories = client.venues.categories()
+        for api in self.apis:
+            categories = api.venues.categories()
             assert 'categories' in categories, u"'categories' not in response"
             assert len(categories['categories']) > 1, u'Expected multiple categories'
