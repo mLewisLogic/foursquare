b29666cf824b923d902c49f7911af64a6ae2c35d
Mike Lewis <mlewis.mail@gmail.com>
2014-01-20 16:22:46 -0800

    version bump to 20140118

--- foursquare/__init__.py
+++ foursquare/__init__.py
@@ -49,9 +49,9 @@ if NETWORK_DEBUG:
 
 
 # Default API version. Move this forward as the library is maintained and kept current
-API_VERSION_YEAR  = '2013'
-API_VERSION_MONTH = '12'
-API_VERSION_DAY   = '17'
+API_VERSION_YEAR  = '2014'
+API_VERSION_MONTH = '01'
+API_VERSION_DAY   = '18'
 API_VERSION = '{year}{month}{day}'.format(year=API_VERSION_YEAR, month=API_VERSION_MONTH, day=API_VERSION_DAY)
 
 # Library versioning matches supported foursquare API version
